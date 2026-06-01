e89a675195bf86b938dd97a10ac1dc1f50111bc8
ankitagupta84 <ankitagupta843329@gmail.com>
2026-06-01 17:24:34 +0530

    Update Q2.cpp

--- Day-1/Q2.cpp
+++ Day-1/Q2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 //program to Print multiplication table of a given number  
 int main(){
     int n;
-    cout<<"Enter the value of N to print the multiplication table: ";
+    cout<<"Enter the value to print the multiplication table: ";
     cin>>n;
     for(int i=1;i<=10;i++){
         cout<<n<<" * "<<i<<" = "<<n*i<<endl;
